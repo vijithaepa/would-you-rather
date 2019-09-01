36ce13e820d9b51769ae43d2a5db1453edb03356
vijitha epa <vepa@bglcorp.com.au>
2019-09-01 20:59:43 +1000

    project structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+
+class App extends Component {
+
+    render() {
+
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
+}
+
+
+export default App;
